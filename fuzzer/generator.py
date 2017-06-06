6d984ddb84cb214b2ba3e1a2f98900ec7165a029
Jay Bosamiya <jaybosamiya@gmail.com>
2017-06-06 22:49:54 +0530

    simplistic generator

--- fuzzer/generator.py
+++ fuzzer/generator.py
@@ -1,8 +1,32 @@
 import random
+import numpy as np
+
+exclude = ['tile','blackman','diag_indices']
+callables = [a
+             for a in dir(np)
+             if a not in exclude and callable(getattr(np,a))]
+
+data_possibilities = [
+    '"A"', '("A"*0x100)', '("A"*0x1000)',
+    '0', '1', '-1',
+    '(2**32)', '(2**32+1)', '(2**32-1)',
+    '(2**64)', '(2**64+1)', '(2**64-1)',
+    '[]','()','{}', 'set()',
+]
+
+data_types = []
 
 def generate():
-    # TODO: Write a proper generator here
-    return random.choice(["print('a')", 'print "A"'])
+    c = random.choice(callables)
+    vals = ', '.join(
+        random.choice(data_possibilities)
+        for _ in range(random.randint(0, 5))
+    )
+    t = "np.%s(%s)" % (c, vals)
+
+    print ("-----------------------------------\n%s\n--------------" % t)
+
+    return t
 
 def register(t):
     # TODO: Use the generated valid code to seed the fuzzer more
