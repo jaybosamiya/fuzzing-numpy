ee93639ce5bdfc69cb6ea70dcfc9a40899880f81
Jay Bosamiya <jaybosamiya@gmail.com>
2017-06-06 20:03:31 +0530

    Set ASAN options

--- wrapper.sh
+++ wrapper.sh
@@ -11,4 +11,9 @@ cd ..
 echo '>>> Set up "crashes/" directory to store crashes'
 mkdir crashes/
 
+echo '>>> Set up ASAN configuration options'
+export ASAN_OPTIONS=handle_segv=0,abort_on_error=1
+# handle_segv=0     == allows user to set their own handler (needed for harness)
+# abort_on_error=1  == causes a SIGABRT upon ASAN error (needed for harness)
+
 echo '>>> TODO Run actual fuzzer'
