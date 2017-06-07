3be4e42e6f48742a9dfdc812a5b86d61d2eebbf5
Jay Bosamiya <jaybosamiya@gmail.com>
2017-06-07 09:12:58 +0530

    Add long-run killer

--- wrapper.sh
+++ wrapper.sh
@@ -16,6 +16,17 @@ export ASAN_OPTIONS=handle_segv=0,abort_on_error=1
 # handle_segv=0     == allows user to set their own handler (needed for harness)
 # abort_on_error=1  == causes a SIGABRT upon ASAN error (needed for harness)
 
+echo '>>> Set up long-run killer (WARNING: Will kill all python processes every few seconds)'
+touch DELETE_THIS_TO_STOP_KILLER
+while true; do
+    sleep 30
+    if test -e DELETE_THIS_TO_STOP_KILLER; then
+        killall python
+    else
+        break
+    fi
+done &
+
 echo '>>> Run actual fuzzer'
 while true; do
       python fuzzer/main.py
